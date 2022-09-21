60c5ee382e872b2592f20e72fb136f96c3ab01d9
Mateus Haupt <104838329+mateushaupt@users.noreply.github.com>
2022-09-21 08:05:24 -0300

    aa folou

--- resources/views/eleitores/comprovante.blade.php
+++ resources/views/eleitores/comprovante.blade.php
@@ -1,11 +1,25 @@
 @extends('base.index')
 
 @section('container')
-<div class="container-xl">
+
+<div class="card text-center" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Comprovante de votação {{$dados[0]->periodo}}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">{{$dados[0]->dt_inicio}}</h6>
+    <p class="card-text">{{$dados[0]->eleitor}}</p>
+    <p class="card-text">Inscrição: {{$dados[0]->titulo}}</p>
+    <p class="card-text">Zona: {{$dados[0]->zona}} Seção: {{$dados[0]->secao}}</p>
+  </div>
+</div>
+
+
+
+
+<!-- <div class="container-xl">
 <h2 style="text-align:center;">Comprovante de votação {{$dados[0]->periodo}}</h2>
 <h1 style="text-align:center;">{{$dados[0]->dt_inicio}}</h1>
 <h2 style="text-align:center;">{{$dados[0]->eleitor}}
 <h3 style="text-align:center;">Inscrição: {{$dados[0]->titulo}}</h3>
 <h3 style="text-align:center;">Zona: {{$dados[0]->zona}} Seção: {{$dados[0]->secao}}</h3>
-</div>
+</div> -->
 @endsection
